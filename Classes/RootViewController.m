c15195eedcfdc221effdb39be7178ddb86b5c94a
Alex Wayne <alex@beautifulpixel.com>
2008-08-02 13:15:19 -0700

    Disabled landscape for now.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -137,7 +137,7 @@
 
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-	// Return YES for supported orientations
+  return (interfaceOrientation == UIInterfaceOrientationPortrait);
 	return (interfaceOrientation == UIInterfaceOrientationPortrait || interfaceOrientation == UIInterfaceOrientationLandscapeLeft || interfaceOrientation == UIInterfaceOrientationLandscapeRight);
 }
 
