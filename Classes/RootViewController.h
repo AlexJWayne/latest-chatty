0f555d5cfa3a27b31a0f67147654cdf51621b778
Alex Wayne <alex@beautifulpixel.com>
2008-08-12 17:44:16 -0700

    Loading indicators are no longer on refresh buttons.  The network avtivity indicator in the status bar should be enough for refreshes.

--- Classes/RootViewController.h
+++ Classes/RootViewController.h
@@ -15,8 +15,6 @@
 
 @interface RootViewController : UITableViewController {
   Feed *feed;
-  UIBarButtonItem *refreshButton;
-  UIBarButtonItem *refreshButtonLoading;
 }
 
 - (void)feedDidFinishLoading;
