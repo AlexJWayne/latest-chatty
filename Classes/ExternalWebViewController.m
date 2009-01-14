e65b5403a515ae67cbe7a46ac753f1ed8b7037b3
Alex Wayne <alex@beautifulpixel.com>
2009-01-14 14:22:58 -0800

    Web view is now landscape freindly.

--- Classes/ExternalWebViewController.m
+++ Classes/ExternalWebViewController.m
@@ -52,7 +52,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	// Return YES for supported orientations
-	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 
